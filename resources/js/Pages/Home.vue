b6fde22a0c4f680aa8026658dc8d766f6a2bfa3f
Angelo Schuler Piletti <74366173+AngeloSchulerPiletti@users.noreply.github.com>
2021-05-18 15:41:12 -0300

    Project Started

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <layout>
+        <main>
+            <h1>Este é o main</h1>
+        </main>
+    </layout>
+</template>
+
+<script>
+import App from "./Layouts/App";
+export default {
+    components: {
+        layout: App,
+    },
+};
+</script>
+
+<style lang="scss">
+h1{
+    color: red;
+}
+</style>
---
+++ resources/js/Pages/Layouts/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <Menu/>
+    <slot/>
+    <Footer/>
+</template>
+
+<script>
+import Menu from "../Components/Menu";
+import Footer from "../Components/Footer";
+export default {
+    components:{
+        Menu,
+        Footer,
+    }
+}
+</script>
+
+<style>
+
+</style>
